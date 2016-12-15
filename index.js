9a61b890afac7489d883aeb1f20ff57604480858
benjalimm <keymote@gmail.com>
2016-12-15 23:29:32 +1030

    api

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ server.listen(process.env.PORT || 8080, function()
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector
-({ appId:'d7e4d8f3-0299-4210-878d-69ac0a16d1f7', appPassword: 'khpjtpBTCUPfMQNjXuY4bof' });
+var connector = new builder.ChatConnector({
+  appId:'d7e4d8f3-0299-4210-878d-69ac0a16d1f7',
+  appPassword: 'khpjtpBTCUPfMQNjXuY4bof' });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
